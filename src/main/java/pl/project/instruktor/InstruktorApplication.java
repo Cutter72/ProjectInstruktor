b6bc0fb0e6446beb83f75e5648ca9d1bb787f30b
Krystian Woźniak <krys.blu@gmail.com>
2019-02-11 10:35:56 +0100

    First configuration

---
+++ src/main/java/pl/project/instruktor/InstruktorApplication.java
@@ -0,0 +1,14 @@
+package pl.project.instruktor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstruktorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstruktorApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/pl/project/instruktor/InstruktorApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.project.instruktor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InstruktorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
