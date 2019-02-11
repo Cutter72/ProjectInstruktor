d6f020986e556c584ec0b7208bf65fb4c275add8
Cutter72 <drelich_pawel@o2.pl>
2019-02-11 12:09:44 +0100

    Added validation to class fields

--- src/main/java/pl/project/instruktor/model/Province.java
+++ src/main/java/pl/project/instruktor/model/Province.java
@@ -1,15 +1,19 @@
 package pl.project.instruktor.model;
 
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
+@Data
 @NoArgsConstructor
 @Table(name = "provinces")
 public class Province {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String name;
 }
